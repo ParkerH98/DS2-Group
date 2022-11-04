613ee539682492b4ad529f45126d3d1b7baef3f5
Parker Hague <parkerhague@gmail.com>
2022-11-04 02:30:37 -0500

    output csv w avg runtimes

--- query5.py
+++ query5.py
@@ -103,8 +103,19 @@ def mergeSort_col2(myList, col):
 
 
 def gather_runtimes(num_runs):
+    runtime_start = time.time()
+    runtimes = multi_run_query(num_runs)
+    runtime_end = time.time()
+    print("time to get all runtimes: " + str(runtime_end-runtime_start))
+    print_header("Exporting the output into csv file")
+    
+    runtimes = pd.DataFrame(runtimes, columns=['Avg Runtime'])
+    runtimes.to_csv('avg_runtimes.csv', index = False)
+    # using generated csv in excel to make a plot
+    
+def multi_run_query(num_runs):
     runtimes = []
-    for input_size in range(0, 92000, 1000):
+    for input_size in range(1000, max_len + 1000, 1000):
         average_runtime = 0
         
         for i in range(num_runs):
@@ -116,6 +127,8 @@ def gather_runtimes(num_runs):
             # average_runtime = average_runtime * 10**9
             random.shuffle(final_buyer_list)
         average_runtime /= num_runs
+        print("Input: " + str(len(final_buyer_list[:input_size])) + " Avg Time (secs): " + str(average_runtime))
+        
         runtimes.append(average_runtime)
     return runtimes
 
@@ -123,22 +136,25 @@ if __name__ == '__main__':
     print_header('Reading the data')
     nft_data = pd.read_csv("datasets/nft_dataset.csv")
     nft_data.info()
+    
     print_header('Removing null values from data')
     print(nft_data.isna().sum())
     nft_data.dropna(inplace=True)
+    
     print_header('Grouping data based on buyers')
     grp_nft = nft_data.groupby('Buyer').agg({'NFT': lambda x: x.nunique(), 'Txn Hash': ['count']}).reset_index()
     grp_nft.columns = [' '.join(str(i) for i in col) for col in grp_nft.columns]
     grp_nft.reset_index(inplace=True)
     grp_nft = grp_nft.drop('index', axis=1)
     grp_nft.rename(
-        
         columns={'Buyer ': 'Buyer', 'Txn Hash count': 'No of Transactions', 'NFT <lambda>': 'Different NFTs'},
         inplace=True)
     print(grp_nft.head())
+    
     print_header("Converting dataframe to list")
     final_buyer_list = grp_nft.values.tolist()
     final_buyer_list
+    
     print_header("Starting the sorting")
     start = time.time()
     mergeSort(final_buyer_list, 1)
@@ -152,12 +168,15 @@ if __name__ == '__main__':
     print(sorted_buyer_data.info)
     print_header("Exporting the output into csv file")
     sorted_buyer_data.to_csv('sorted_buyer_dataset.csv')
+    
+    max_len = round(len(final_buyer_list) / 1000) * 1000
+    input_intervals = np.arange(1000, max_len + 1000, 1000)
 
-    runtimes = gather_runtimes(100)
-    input_intervals = np.arange(1000, 93000, 1000)
-
-    plt.plot(input_intervals, runtimes)
-    plt.xlabel("Input Size")
-    plt.ylabel("Runtime (Seconds)")
-    plt.show()
+    # runs the dataset with various sizes & outputs the results in a csv
+    # gather_runtimes(100)
+    
+    # plt.plot(input_intervals, runtimes)
+    # plt.xlabel("Input Size")
+    # plt.ylabel("Runtime (ns)")
+    # plt.show()
     
